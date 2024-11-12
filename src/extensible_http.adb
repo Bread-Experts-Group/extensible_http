b1463cafeb9ddfcc8da8fc99ea6185db0e2240b4
Miko Elbrecht <tcgytsc@gmail.com>
2024-11-11 23:49:57 -0600

    buildAda adoption

--- src/extensible_http.adb
+++ src/extensible_http.adb
@@ -1,4 +1,5 @@
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Strings.Unbounded;
+use Ada.Strings.Unbounded;
 
 package body Extensible_HTTP is
 
@@ -6,19 +7,16 @@ package body Extensible_HTTP is
 
    use Field_Hashed_Maps;
 
-   function Read_String_From_Stream
-     (Stream : not null access Root_Stream_Type'Class; Delimiter : String)
-      return String
-   is
+   function Read_String_From_Stream (Stream : not null access Root_Stream_Type'Class; Delimiter : String) return String is
+
       Read_Into : Unbounded_String;
       Read      : Unbounded_String;
+
    begin
       loop
          Read.Append (Character'Input (Stream));
-         if Read.Length >= Delimiter'Length
-           and then
-             Read.Slice (Read.Length - Delimiter'Length + 1, Read.Length) =
-             Delimiter
+
+         if Read.Length >= Delimiter'Length and then Read.Slice (Read.Length - Delimiter'Length + 1, Read.Length) = Delimiter
          then
             Read_Into.Append (Read.Slice (1, Read.Length - Delimiter'Length));
             exit;
@@ -27,11 +25,10 @@ package body Extensible_HTTP is
       return Read_Into.To_String;
    end Read_String_From_Stream;
 
-   procedure HTTP_11_Status_Image
-     (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class;
-      Value  :        HTTP_11_Status_Code)
-   is
+   procedure HTTP_11_Status_Image (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class; Value : HTTP_11_Status_Code) is
+
       As_String : constant String := Integer (Value)'Image;
+
    begin
       Output.Put (As_String (2 .. As_String'Last));
    end HTTP_11_Status_Image;
@@ -41,60 +38,73 @@ package body Extensible_HTTP is
    -- HTTP/1.1 Message --
    ----------------------
 
-   procedure Write_HTTP_11_Message_Body
-     (Stream : not null access Root_Stream_Type'Class; Item : HTTP_11_Message)
-   is
+   procedure Write_HTTP_11_Message_Body (Stream : not null access Root_Stream_Type'Class; Item : HTTP_11_Message) is
    begin
       String'Write (Stream, CRLF);
-      for E in Item.Fields.Iterate loop
+
+      for E in Item.Fields.Iterate
+      loop
          Token'Write (Stream, Key (E));
          String'Write (Stream, ':' & Element (E) & CRLF);
       end loop;
       String'Write (Stream, CRLF);
-      if not Item.Message_Body.Is_Empty then
+
+      if not Item.Message_Body.Is_Empty
+      then
          String'Write (Stream, Item.Message_Body.Element);
       end if;
    end Write_HTTP_11_Message_Body;
 
-   ----------------------
-   -- BASE / READ      --
-   -- HTTP/1.1 Message --
-   ----------------------
+      ----------------------
+      -- BASE / READ      --
+      -- HTTP/1.1 Message --
+      ----------------------
 
-   procedure Read_HTTP_11_Message_Body
-     (Stream :     not null access Root_Stream_Type'Class;
-      Item   : out HTTP_11_Message)
-   is
+   procedure Read_HTTP_11_Message_Body (Stream : not null access Root_Stream_Type'Class; Item : out HTTP_11_Message) is
+
       CRLF_Tag : String (1 .. 2);
+
    begin
       String'Read (Stream, CRLF_Tag);
+
       loop
          String'Read (Stream, CRLF_Tag);
-         if CRLF_Tag /= CRLF then
+
+         if CRLF_Tag /= CRLF
+         then
             declare
-               Read_Token : constant Token :=
-                 Token (CRLF_Tag & Read_String_From_Stream (Stream, ":"));
-               Read_Content : constant String :=
-                 Read_String_From_Stream (Stream, CRLF);
+
+               Read_Token   : constant Token  := Token (CRLF_Tag & Read_String_From_Stream (Stream, ":"));
+               Read_Content : constant String := Read_String_From_Stream (Stream, CRLF);
+
             begin
                Item.Fields.Include (Read_Token, Read_Content);
             end;
+
          else
             exit;
          end if;
       end loop;
-      if Item.Fields.Contains ("Transfer-Encoding") then
+
+      if Item.Fields.Contains ("Transfer-Encoding")
+      then
          raise Program_Error with "TE!!";
-      elsif Item.Fields.Contains ("Content-Length") then
+
+      elsif Item.Fields.Contains ("Content-Length")
+      then
          declare
-            Content_Length : Natural :=
-              Natural'Value (Item.Fields.Element ("Content-Length"));
-            Message_Body : Unbounded_String;
+
+            Content_Length : Natural := Natural'Value (Item.Fields.Element ("Content-Length"));
+            Message_Body   : Unbounded_String;
+
          begin
             loop
                exit when Content_Length = 0;
+
                declare
+
                   Chunked : String (1 .. Natural'Min (Content_Length, 8_128));
+
                begin
                   String'Read (Stream, Chunked);
                   Message_Body.Append (Chunked);
@@ -106,53 +116,50 @@ package body Extensible_HTTP is
       end if;
    end Read_HTTP_11_Message_Body;
 
-   ----------------------
-   -- REQUEST / WRITE  --
-   -- HTTP/1.1 Message --
-   ----------------------
+      ----------------------
+      -- REQUEST / WRITE  --
+      -- HTTP/1.1 Message --
+      ----------------------
 
-   procedure Write_HTTP_11_Request_Message
-     (Stream : not null access Root_Stream_Type'Class;
-      Item   : HTTP_11_Request_Message)
-   is
-      RequestLine : constant String :=
-        Item.Method'Image & ' ' & Item.Target.Element & " HTTP/1.1";
+   procedure Write_HTTP_11_Request_Message (Stream : not null access Root_Stream_Type'Class; Item : HTTP_11_Request_Message) is
+
+      RequestLine : constant String := Item.Method'Image & ' ' & Item.Target.Element & " HTTP/1.1";
+
    begin
       String'Write (Stream, RequestLine);
       Write_HTTP_11_Message_Body (Stream, Item);
    end Write_HTTP_11_Request_Message;
 
-   ----------------------
-   -- REQUEST / READ   --
-   -- HTTP/1.1 Message --
-   ----------------------
+      ----------------------
+      -- REQUEST / READ   --
+      -- HTTP/1.1 Message --
+      ----------------------
 
-   procedure Read_HTTP_11_Request_Message
-     (Stream :     not null access Root_Stream_Type'Class;
-      Item   : out HTTP_11_Request_Message)
-   is
+   procedure Read_HTTP_11_Request_Message (Stream : not null access Root_Stream_Type'Class; Item : out HTTP_11_Request_Message) is
+
       discard : String (1 .. 8);
+
    begin
-      Item.Method :=
-        HTTP_11_Method_Types'Value (Read_String_From_Stream (Stream, " "));
+      Item.Method := HTTP_11_Method_Types'Value (Read_String_From_Stream (Stream, " "));
       Item.Target.Replace_Element (Read_String_From_Stream (Stream, " "));
       String'Read (Stream, discard);
       Read_HTTP_11_Message_Body (Stream, Item);
    end Read_HTTP_11_Request_Message;
 
-   ----------------------
-   -- RESPONSE / WRITE --
-   -- HTTP/1.1 Message --
-   ----------------------
+      ----------------------
+      -- RESPONSE / WRITE --
+      -- HTTP/1.1 Message --
+      ----------------------
 
-   procedure Write_HTTP_11_Response_Message
-     (Stream : not null access Root_Stream_Type'Class;
-      Item   : HTTP_11_Response_Message)
-   is
+   procedure Write_HTTP_11_Response_Message (Stream : not null access Root_Stream_Type'Class; Item : HTTP_11_Response_Message) is
+
       StatusLine : constant String := "HTTP/1.1 " & Item.Status'Image & ' ';
+
    begin
       String'Write (Stream, StatusLine);
-      if not Item.Reason.Is_Empty then
+
+      if not Item.Reason.Is_Empty
+      then
          String'Write (Stream, Item.Reason.Element);
       end if;
       Write_HTTP_11_Message_Body (Stream, Item);
@@ -163,10 +170,7 @@ package body Extensible_HTTP is
    -- HTTP/1.1 Message --
    ----------------------
 
-   procedure Read_HTTP_11_Response_Message
-     (Stream :     not null access Root_Stream_Type'Class;
-      Item   : out HTTP_11_Response_Message)
-   is
+   procedure Read_HTTP_11_Response_Message (Stream : not null access Root_Stream_Type'Class; Item : out HTTP_11_Response_Message) is
    begin
       null;
    end Read_HTTP_11_Response_Message;
--- src/extensible_http.ads
+++ src/extensible_http.ads
@@ -2,128 +2,133 @@ with Ada.Containers.Indefinite_Ordered_Maps;
 with Ada.Containers.Indefinite_Holders;
 with Ada.Strings.Text_Buffers;
 
-with Ada.Streams; use Ada.Streams;
+with Ada.Strings.UTF_Encoding;
+use Ada.Strings.UTF_Encoding;
+
+with Ada.Streams;
+use Ada.Streams;
 
 package Extensible_HTTP is
+   type Protocol_Types is
+     (HTTP,
+      HTTPS);
 
-   type Protocol_Types is (HTTP, HTTPS);
+     --  https://datatracker.ietf.org/doc/html/rfc9110#name-methods, Table 4
+     --  * Essential methods must always be available.
+     --  * Non-essential methods can be disabled.
+     --   - Methods that are disabled/unimplemented must:
+     --    - Reply with a 501.
+     --    - If they are disallowed, reply with a 405.
+     --  * Safe methods only do "read-only" operations on the server.
+     --   - https://datatracker.ietf.org/doc/html/rfc9110#name-safe-methods
+     --  * Idempotent methods will have the same effect across identical requests.
+     --   - Safe methods are idempotent.
+     --   - https://datatracker.ietf.org/doc/html/rfc9110#name-idempotent-methods
+     --  @value GET Safe, essential
+     --  @value HEAD Safe, essential
+     --  @value POST Unsafe, non-idempotent, non-essential
+     --  @value PUT Unsafe, idempotent, non-essential
+     --  @value DELETE Unsafe, idempotent, non-essential
+     --  @value CONNECT Unsafe, non-idempotent, non-essential
+     --  @value OPTIONS Safe, non-essential
+     --  @value TRACE Safe, non-essential
 
-   --  https://datatracker.ietf.org/doc/html/rfc9110#name-methods, Table 4
-   --  * Essential methods must always be available.
-   --  * Non-essential methods can be disabled.
-   --   - Methods that are disabled/unimplemented must:
-   --    - Reply with a 501.
-   --    - If they are disallowed, reply with a 405.
-   --  * Safe methods only do "read-only" operations on the server.
-   --   - https://datatracker.ietf.org/doc/html/rfc9110#name-safe-methods
---  * Idempotent methods will have the same effect across identical requests.
-   --   - Safe methods are idempotent.
-   --   - https://datatracker.ietf.org/doc/html/rfc9110#name-idempotent-methods
-   --  @value GET Safe, essential
-   --  @value HEAD Safe, essential
-   --  @value POST Unsafe, non-idempotent, non-essential
-   --  @value PUT Unsafe, idempotent, non-essential
-   --  @value DELETE Unsafe, idempotent, non-essential
-   --  @value CONNECT Unsafe, non-idempotent, non-essential
-   --  @value OPTIONS Safe, non-essential
-   --  @value TRACE Safe, non-essential
    type HTTP_11_Method_Types is
-     (GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE);
+     (GET,
+      HEAD,
+      POST,
+      PUT,
+      DELETE,
+      CONNECT,
+      OPTIONS,
+      TRACE);
 
-   --  TODO: Token character limits
+     --  TODO: Token character limits
+
    type Token is new String;
 
-   --  https://datatracker.ietf.org/doc/html/rfc5234
-   --  Defined as "VCHAR"
-   type Visible_Character is
-     new Character range Character'Val (16#21#) .. Character'Val (16#7E#);
+      --  https://datatracker.ietf.org/doc/html/rfc5234
+      --  Defined as "VCHAR"
 
-   --  https://datatracker.ietf.org/doc/html/rfc9110#name-field-values
-   --  Defined as "obs-text"
-   type OBS_Character is
-     new Character range Character'Val (16#80#) .. Character'Val (16#FF#);
+   type Visible_Character is new Character range Character'Val (16#21#) .. Character'Val (16#7E#);
 
-   --  https://datatracker.ietf.org/doc/html/rfc9110#name-field-values
-   --  Defined as "field-vchar"
-   type Field_Visible_Character is
-     new Character range Character (Visible_Character'First) ..
-         Character (OBS_Character'Last) with
-      Predicate => Character (Field_Visible_Character) /=
-      Character'Val (16#7F#);
+      --  https://datatracker.ietf.org/doc/html/rfc9110#name-field-values
+      --  Defined as "obs-text"
 
-      --  https://datatracker.ietf.org/doc/html/rfc9110#name-collected-abnf
-      --  Defined as "OWS", "RWS", "BWS" (Optional, Required, Bad) White Space
+   type OBS_Character is new Character range Character'Val (16#80#) .. Character'Val (16#FF#);
+
+      --  https://datatracker.ietf.org/doc/html/rfc9110#name-field-values
+      --  Defined as "field-vchar"
+
+   type Field_Visible_Character is new Character range Character (Visible_Character'First) .. Character (OBS_Character'Last) with
+     Predicate => Character (Field_Visible_Character) /= Character'Val (16#7F#);
+
+   --  https://datatracker.ietf.org/doc/html/rfc9110#name-collected-abnf
+   --  Defined as "OWS", "RWS", "BWS" (Optional, Required, Bad) White Space
+
    type White_Space is new Character range ' ' .. ASCII.HT;
 
    package String_Holders is new Ada.Containers.Indefinite_Holders (String);
+   package UTF_String_Holders is new Ada.Containers.Indefinite_Holders (UTF_8_String);
 
-   package Field_Hashed_Maps is new Ada.Containers.Indefinite_Ordered_Maps
-     (Key_Type => Token, Element_Type => String);
+   package Field_Hashed_Maps is new Ada.Containers.Indefinite_Ordered_Maps (Key_Type => Token, Element_Type => String);
 
-   --  https://datatracker.ietf.org/doc/html/rfc9112#name-message
-   --  HTTP/1.1 HTTP-message
-   type HTTP_11_Message is abstract tagged record
-      Fields       : Field_Hashed_Maps.Map;
-      Message_Body : String_Holders.Holder := String_Holders.Empty_Holder;
-   end record;
+     --  https://datatracker.ietf.org/doc/html/rfc9112#name-message
+     --  HTTP/1.1 HTTP-message
 
-   procedure Write_HTTP_11_Message_Body
-     (Stream : not null access Root_Stream_Type'Class; Item : HTTP_11_Message);
+   type HTTP_11_Message is
+     abstract tagged record
+        Fields       : Field_Hashed_Maps.Map;
+        Message_Body : UTF_String_Holders.Holder := UTF_String_Holders.Empty_Holder;
+     end record;
 
-   procedure Read_HTTP_11_Message_Body
-     (Stream :     not null access Root_Stream_Type'Class;
-      Item   : out HTTP_11_Message);
+   procedure Write_HTTP_11_Message_Body (Stream : not null access Root_Stream_Type'Class; Item : HTTP_11_Message);
 
-   --  https://datatracker.ietf.org/doc/html/rfc9112#name-request-line
-   --  HTTP/1.1 request-line
-   --  @field Target The request target for this request. TODO accompany forms
-   type HTTP_11_Request_Message is new HTTP_11_Message with record
-      Method : HTTP_11_Method_Types;
-      Target : String_Holders.Holder;
-   end record;
+   procedure Read_HTTP_11_Message_Body (Stream : not null access Root_Stream_Type'Class; Item : out HTTP_11_Message);
 
-   --  https://datatracker.ietf.org/doc/html/rfc9112#name-status-line
-   --  HTTP/1.1 status-code; NOTE: The 'Image of this type has no sign space.
+     --  https://datatracker.ietf.org/doc/html/rfc9112#name-request-line
+     --  HTTP/1.1 request-line
+     --  @field Target The request target for this request. TODO accompany forms
+
+   type HTTP_11_Request_Message is
+     new HTTP_11_Message with record
+        Method : HTTP_11_Method_Types;
+        Target : String_Holders.Holder;
+     end record;
+
+     --  https://datatracker.ietf.org/doc/html/rfc9112#name-status-line
+     --  HTTP/1.1 status-code; NOTE: The 'Image of this type has no sign space.
+
    type HTTP_11_Status_Code is range 100 .. 999 with
-      Put_Image => HTTP_11_Status_Image;
+     Put_Image => HTTP_11_Status_Image;
 
-   procedure HTTP_11_Status_Image
-     (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class;
-      Value  :        HTTP_11_Status_Code);
+   procedure HTTP_11_Status_Image (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class; Value : HTTP_11_Status_Code);
 
-   --  https://datatracker.ietf.org/doc/html/rfc9112#name-status-line
-   --  HTTP/1.1 status-line
-   --  @field Status The three digit status code in response to the previous
-   --  request.
-   --  @field Reason A reason phrase associated with the status code.
-   --  By default, this is null.
-   type HTTP_11_Response_Message is new HTTP_11_Message with record
-      Status : HTTP_11_Status_Code;
-      Reason : String_Holders.Holder := String_Holders.Empty_Holder;
-   end record;
+     --  https://datatracker.ietf.org/doc/html/rfc9112#name-status-line
+     --  HTTP/1.1 status-line
+     --  @field Status The three digit status code in response to the previous
+     --  request.
+     --  @field Reason A reason phrase associated with the status code.
+     --  By default, this is null.
+
+   type HTTP_11_Response_Message is
+     new HTTP_11_Message with record
+        Status : HTTP_11_Status_Code;
+        Reason : String_Holders.Holder := String_Holders.Empty_Holder;
+     end record;
 
 private
+   procedure Write_HTTP_11_Request_Message (Stream : not null access Root_Stream_Type'Class; Item : HTTP_11_Request_Message);
 
-   procedure Write_HTTP_11_Request_Message
-     (Stream : not null access Root_Stream_Type'Class;
-      Item   : HTTP_11_Request_Message);
-
-   procedure Read_HTTP_11_Request_Message
-     (Stream :     not null access Root_Stream_Type'Class;
-      Item   : out HTTP_11_Request_Message);
+   procedure Read_HTTP_11_Request_Message (Stream : not null access Root_Stream_Type'Class; Item : out HTTP_11_Request_Message);
 
    for HTTP_11_Request_Message'Read use Read_HTTP_11_Request_Message;
    for HTTP_11_Request_Message'Write use Write_HTTP_11_Request_Message;
 
-   procedure Write_HTTP_11_Response_Message
-     (Stream : not null access Root_Stream_Type'Class;
-      Item   : HTTP_11_Response_Message);
+   procedure Write_HTTP_11_Response_Message (Stream : not null access Root_Stream_Type'Class; Item : HTTP_11_Response_Message);
 
-   procedure Read_HTTP_11_Response_Message
-     (Stream :     not null access Root_Stream_Type'Class;
-      Item   : out HTTP_11_Response_Message);
+   procedure Read_HTTP_11_Response_Message (Stream : not null access Root_Stream_Type'Class; Item : out HTTP_11_Response_Message);
 
    for HTTP_11_Response_Message'Read use Read_HTTP_11_Response_Message;
    for HTTP_11_Response_Message'Write use Write_HTTP_11_Response_Message;
-
 end Extensible_HTTP;
